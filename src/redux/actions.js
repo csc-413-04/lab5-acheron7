9c0e03181ae03101f000568bcf0094e1fc51d649
David <dadams03@gmail.com>
2018-11-29 21:25:50 -0800

    End of day commit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,3 +2,13 @@ export const doTest = () => ({
     type: 'SET_TEST',
     test: 'some text',
 });
+
+export const loadAllMessages = (message) => ({
+    type: 'LOAD_MESSAGES',
+    messages,
+});
+
+export const importMessage = (message) => ({
+    type: 'IMPORT_MESSAGE',
+    message,
+});
